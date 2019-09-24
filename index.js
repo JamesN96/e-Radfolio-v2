6e9220629cb7fc5d12be02674897486f43a88c0c
James Nguyen <james.nguyenn96@gmail.com>
2019-09-24 12:44:11 +1000

    Create two new functions named showDiv2 and showDiv3. Set these two new functions as callbacks for btn2 and btn3 eventlisteneres

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// MRPBA SECTION 
+
 // DOM Selectors
 
 let btn1 = document.getElementById("btn-1"); 
@@ -6,12 +8,20 @@ let btn3 = document.getElementById("btn-3");
 
 // Event Listeners
 btn1.addEventListener("click", showDiv);
-btn2.addEventListener("click", showDiv);
-btn3.addEventListener("click", showDiv);
+btn2.addEventListener("click", showDiv2);
+btn3.addEventListener("click", showDiv3);
 
 
 // showDiv function
 
 function showDiv(e) {
     document.getElementById("div-1").classList.toggle("display-none"); 
+}
+
+function  showDiv2(e) {
+    document.getElementById("div-2").classList.toggle("display-none"); 
+}
+
+function  showDiv3(e) {
+    document.getElementById("div-3").classList.toggle("display-none"); 
 }
