e343867287a96f449192aea07d1700e0dd0c937c
James Nguyen <james.nguyenn96@gmail.com>
2019-09-24 13:08:43 +1000

    Add another two dom selectors for the buttons inside div-2 and div-3. Give those selectors an event listener each to toggle their respective showDiv function

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ let btn1 = document.getElementById("btn-1");
 let btn2 = document.getElementById("btn-2");
 let btn3 = document.getElementById("btn-3");
 let close1 = document.getElementById("close-1");
+let close2 = document.getElementById("close-2");
+let close3 = document.getElementById("close-3");
+
 
 // Event Listeners
 btn1.addEventListener("click", showDiv);
 btn2.addEventListener("click", showDiv2);
 btn3.addEventListener("click", showDiv3);
 close1.addEventListener("click", showDiv);
+close2.addEventListener("click", showDiv2);
+close3.addEventListener("click", showDiv3);
+
 
 
 // showDiv function
