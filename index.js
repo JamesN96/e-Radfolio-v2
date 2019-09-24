9cfe9ba8873b25d6fc074904c525f02cb9b7a739
James Nguyen <james.nguyenn96@gmail.com>
2019-09-24 12:34:22 +1000

    Add two more dom element selector for two remaining buttons

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Selectors
 
 let btn1 = document.getElementById("btn-1"); 
+let btn2 = document.getElementById("btn-2");
+let btn3 = document.getElementById("btn-3");
+
 
 btn1.addEventListener("click", showDiv);
 
