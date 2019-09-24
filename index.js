dd0e48add3d7b0bff6d96520dcfad5ebde8c32cb
James Nguyen <james.nguyenn96@gmail.com>
2019-09-24 12:41:12 +1000

    Add two more event listeners to btn2 and btn3. Add in some comments to separate different sections

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-// Selectors
+// DOM Selectors
 
 let btn1 = document.getElementById("btn-1"); 
 let btn2 = document.getElementById("btn-2");
 let btn3 = document.getElementById("btn-3");
 
-
+// Event Listeners
 btn1.addEventListener("click", showDiv);
+btn2.addEventListener("click", showDiv);
+btn3.addEventListener("click", showDiv);
+
 
 // showDiv function
 
