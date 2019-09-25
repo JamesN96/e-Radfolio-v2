fea61ad0434e9b5d6ba424cf9555082ac3b412ba
James Nguyen <james.nguyenn96@gmail.com>
2019-09-25 13:27:04 +1000

    Rewrite the showArticle functions to include if statements that checks whether an article element is showing or not, so that no more than one article element is showing

--- index.js
+++ index.js
@@ -15,18 +15,37 @@ reflect.addEventListener("click", showArticle);
 self.addEventListener("click", showArticle2);
 judge.addEventListener("click", showArticle3);
 
+
 // Function
 
 function showArticle(e) {
-    learning.classList.toggle("display-none");
+    if(learning2.classList.contains("display-none") && learning3.classList.contains("display-none")) {
+        learning.classList.toggle("display-none");
+    } else {
+        learning2.classList.add("display-none");
+        learning3.classList.add("display-none");
+        learning.classList.toggle("display-none");
+    }
 }
 
 function showArticle2(e) {
-    learning2.classList.toggle("display-none");
+    if(learning.classList.contains("display-none") && learning3.classList.contains("display-none")) {
+        learning2.classList.toggle("display-none");
+    } else {
+        learning.classList.add("display-none");
+        learning3.classList.add("display-none");
+        learning2.classList.toggle("display-none");
+    }
 }
 
 function showArticle3(e) {
-    learning3.classList.toggle("display-none");
+    if(learning.classList.contains("display-none") && learning2.classList.contains("display-none")) {
+        learning3.classList.toggle("display-none");
+    } else {
+        learning.classList.add("display-none");
+        learning2.classList.add("display-none");
+        learning3.classList.toggle("display-none");
+    }
 }
 
 // MRPBA SECTION 
